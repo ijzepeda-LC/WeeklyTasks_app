214c8f5f818a67cdf844175df42ca56f6f025b28
Ivan Zepeda <122490453+ijzepeda-LC@users.noreply.github.com>
2023-11-02 17:52:53 -0400

    second commit

---
+++ app/src/main/java/com/ma1s1/your_name_here/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.ma1s1.your_name_here
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
